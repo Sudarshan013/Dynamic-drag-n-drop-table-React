d7c5d9f72a05c938aeb5703b2d94186860fa5e84
Sudarshan <sudharshan@tagalys.com>
2020-04-30 22:43:21 +0530

    misc

--- src/test.js
+++ src/test.js
@@ -1,31 +1,31 @@
 export const testData = {
   available_header: [
     {
-      name: 'Label',
+      name: 'Name',
       value: 'header1',
     },
     {
-      name: 'Searchable',
+      name: 'Gender',
       value: 'header2',
     },
     {
-      name: 'Autocomplete',
+      name: 'Location',
       value: 'header3',
     },
     {
-      name: 'Spellcheck',
+      name: 'Age',
       value: 'header4',
     },
     {
-      name: 'Display',
+      name: 'Nationality',
       value: 'header5',
     },
     {
-      name: 'Available for frontend display',
+      name: 'Occupation',
       value: 'header6',
     },
     {
-      name: 'Available for Dashboard filters',
+      name: 'Hobby',
       value: 'header7',
     },
   ],
@@ -33,109 +33,61 @@ export const testData = {
 
 export const items = [
   {
-    header1: 'Row 1 header1',
-    header2: 'Row 1 header2',
-    header3: 'Row 1 header3',
-    header4: 'Row 1 header4',
-    header5: 'Row 1 Label Searchable',
-    header6: 'Row 1 header6',
-    header7: 'Row 1 header7',
-    header8: 'Row 1 header8',
-    header9: 'Row 1 header9',
-    header10: 'Row 1 header10',
+    header1: 'Pablo Escobar',
+    header2: 'M',
+    header3: 'Mexico',
+    header4: '42',
+    header5: 'American',
+    header6: 'Drug Mafia',
+    header7: 'Smuggling',
   },
   {
-    header1: 'Row 2 header1',
-    header2: 'Row 2 header2',
-    header3: 'Row 2 header3',
-    header4: 'Row 2 header4',
-    header5: 'Row 2 header 5',
-    header6: 'Row 2 header6',
-    header7: 'Row 2 header7',
-    header8: 'Row 2 header8',
-    header9: 'Row 2 header9',
-    header10: 'Row 2 header10',
+    header1: 'Alberto',
+    header2: 'M',
+    header3: 'Cuba',
+    header4: '24',
+    header5: 'American',
+    header6: 'Drug Mafia',
+    header7: 'Smuggling',
   },
   {
-    header1: 'Row 3 header1',
-    header2: 'Row 3 header2',
-    header3: 'Row 3 header3',
-    header4: 'Row 3 header4',
-    header5: 'Row 3 header 5',
-    header6: 'Row 3 header6',
-    header7: 'Row 3 header7',
-    header8: 'Row 3 header8',
-    header9: 'Row 3 header9',
-    header10: 'Row 3 header10',
+    header1: 'Jessie Pinkman',
+    header2: 'M',
+    header3: 'Panama',
+    header4: '43',
+    header5: 'American' ,
+    header6: 'Chemistry Fanatic',
+    header7: 'Meth Making',
   },
   {
-    header1: 'Row 4 header1',
-    header2: 'Row 4 header2',
-    header3: 'Row 4 header3',
-    header4: 'Row 4 header4',
-    header5: 'Row 4 header 5',
-    header6: 'Row 4 header6',
-    header7: 'Row 4 header7',
-    header8: 'Row 4 header8',
-    header9: 'Row 4 header9',
-    header10: 'Row 4 header10',
+    header1: 'Tata Escobar',
+    header2: 'F',
+    header3: 'Costa Rica',
+    header4: '53',
+    header5: 'American'
+    ,
+    header6: 'Drug Mafia',
+    header7: 'Smuggling',
+
   },
   {
-    header1: 'Row 5 header1',
-    header2: 'Row 5 header2',
-    header3: 'Row 5 header3',
-    header4: 'Row 5 header4',
-    header5: 'Row 5 header 5',
-    header6: 'Row 5 header6',
-    header7: 'Row 5 header7',
-    header8: 'Row 5 header8',
-    header9: 'Row 5 header9',
-    header10: 'Row 5 header10',
+    header1: 'Walter White',
+    header2: 'M',
+    header3: 'Jamaica',
+    header4: '38',
+    header5: 'American',
+    header6: 'Chemistry Proffesor',
+    header7: 'Smuggling'
   },
   {
-    header1: 'Row 6 header1',
-    header2: 'Row 6 header2',
-    header3: 'Row 6 header3',
-    header4: 'Row 6 header4',
-    header5: 'Row 6 header 5',
-    header6: 'Row 6 header6',
-    header7: 'Row 6 header7',
-    header8: 'Row 6 header8',
-    header9: 'Row 6 header9',
-    header10: 'Row 6 header10',
+    header1: 'Steve Murphy',
+    header2: 'M',
+    header3: 'Haiti',
+    header4: '40',
+    header5: 'American'
+    ,
+    header6: 'Drug Mafia',
+    header7: 'Smuggling'
   }
 ]
 
-// items: [
-//   {
-//     id: '123',
-//     label: 'Categories',
-//     suggestions_format: 'Individually, Second part of drill-down',
-//     tags_searchable: 'Yes',
-//     label_searchable: 'Yes',
-//     include_in_api_response: 'No',
-//     actions: {
-//       inline: [
-//         {
-//           name: 'edit',
-//           icon: 'edit',
-//           label: 'Edit',
-//           action: {
-//             type: 'link',
-//             details: {
-//               url: '...',
-//               method: 'GET'
-//             }
-//           }
-//         },
-//         {
-//           name: 'view_report',
-//           icon: 'analytics',
-//           label: 'Report'
-//         }
-//       ],
-//       overflow: [
-//       ]
-//     }
-//   }
-// ]
